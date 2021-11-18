7a96772688ee385b9c975bb8a75f6010345b8d76
Joan Odicio <yusuke.joan@gmail.com>
2021-11-18 22:02:37 +0100

    :lipstick: Added isSelected property in Button.

--- client/src/components/ui/Button.js
+++ client/src/components/ui/Button.js
@@ -1,12 +1,14 @@
-import classes from "./Button.module.css"
+import classes from "./Button.module.css";
 
-const Button = ({name, onClick, className}) => {
+const Button = ({name, onClick, className, isSelected}) => {
+   const selectedClass = isSelected ? classes.selected : "";
    return (
-   <button className={`${classes.button} ${className || ""}` } onClick = {onClick}> 
-      <span>
-         {name}
-      </span>
-   </button>);
+      <button
+         className={`${classes.button} ${className || ""} ${selectedClass}`}
+         onClick={onClick}>
+         <span>{name}</span>
+      </button>
+   );
 };
 
 export default Button;
